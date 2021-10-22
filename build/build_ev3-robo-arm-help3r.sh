36d895ea8b90363b65af325a39a18aa834030a77
Andrew Grankin <andreygrankin@gmail.com>
2021-10-23 00:25:49 +0300

    Reorganisation

---
+++ build/build_ev3-robo-arm-help3r.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# variables definition 
+file_name="controlling_LEDs_with_touch_sensor"
+current_location=`pwd`
+
+# removing the old binary file if it exists
+echo "removing the old binary file if it exists..."
+if [[ -e ${current_location}/${file_name} ]]
+then
+   rm ${current_location}/${file_name}
+fi
+
+# source code compilation
+echo "compiling the ${file_name}.cpp and ev3dev.cpp files..."
+arm-linux-gnueabi-g++ -static ../../ev3dev.cpp ${file_name}.cpp -o ${file_name}
+
+# moving binary file to the brick
+if [[ -e ${current_location}/${file_name} ]]
+then 
+   echo "compilation is ok"
+   echo "moving compiled file to the brick..."
+   scp ${file_name} robot@10.42.0.133:~/scripts/cpp
+else
+   echo "compilation is NOT OK"
+fi
