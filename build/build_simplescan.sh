394879130c5a4256a6448a30e7d7c5b09830e7cd
Andrew Grankin <andreygrankin@gmail.com>
2021-10-23 19:57:20 +0300

    Added diray file and simplescan.cpp bluetooth

--- build/build_simplescan.sh
+++ build/build_simplescan.sh
@@ -5,22 +5,13 @@ file_name="simplescan"
 current_location=`pwd`
 
 # removing the old binary file if it exists
-echo "removing the old binary file if it exists..."
+echo "log: removing the old binary file if it exists..."
 if [[ -e ${current_location}/artifacts/${file_name} ]]
 then
    rm ${current_location}/artifacts/${file_name}
+   echo "log: the file ${file_name} has been removed"
 fi
 
 # source code compilation
-echo "compiling the ${file_name}.cpp"
-arm-linux-gnueabi-g++ -static ../../ev3dev.cpp ${file_name}.cpp -o ${file_name}
-
-# moving binary file to the brick
-if [[ -e ${current_location}/${file_name} ]]
-then 
-   echo "compilation is ok"
-   echo "moving compiled file to the brick..."
-   scp ${file_name} robot@10.42.0.133:~/scripts/cpp
-else
-   echo "compilation is NOT OK"
-fi
+echo "log: compiling the ${file_name}.cpp ..."
+g++ ../bluetooth/${file_name}.cpp -o artifacts/${file_name} -lbluetooth
